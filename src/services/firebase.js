3136fb370f2d0918df823661a6facf575e1c8a99
alba-molina-nyc <avmanyc@gmail.com>
2021-10-30 11:50:59 -0400

    wic: install and config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-        apiKey: "AIzaSyAofdyugncMey83u9Bn-MWs0uesU42bKZ4",
-        authDomain: "career-insights-official.firebaseapp.com",
-        projectId: "career-insights-official",
-        storageBucket: "career-insights-official.appspot.com",
-        messagingSenderId: "476108309483",
-        appId: "1:476108309483:web:75219961732a7f8598cfae"
-      };
-      
+    apiKey: "AIzaSyAofdyugncMey83u9Bn-MWs0uesU42bKZ4",
+    authDomain: "career-insights-official.firebaseapp.com",
+    projectId: "career-insights-official",
+    storageBucket: "career-insights-official.appspot.com",
+    messagingSenderId: "476108309483",
+    appId: "1:476108309483:web:75219961732a7f8598cfae"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 // set up auth
