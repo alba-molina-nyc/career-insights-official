fcb4ae7353cb501b604dfb1b0d085a5f4c6c6860
alba-molina-nyc <avmanyc@gmail.com>
2021-10-30 11:58:19 -0400

    wic added components

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+const Nav = (props) => {
+    return (
+    <Nav>
+        <h1>
+            Dashboard
+        </h1>
+        <h1>
+            Contacts
+        </h1>
+        <h1>
+            Your Applications
+        </h1>
+    </Nav>
+    );
+};
+
+export default Nav;
