29c0f79c1f40f78fb3953555a262f4cac0cfa57b
alba-molina-nyc <avmanyc@gmail.com>
2021-11-01 11:01:30 -0400

    wip: api data showing up on console web

--- src/App.js
+++ src/App.js
@@ -14,20 +14,12 @@ import Applications from './pages/Applications';
 import About from './pages/About';
 import Solutions from './pages/Solutions';
 import Product from './pages/Product';
-import Show from './pages/Show';
+// import Show from './pages/Show';
 
 
 import { auth } from './services/firebase';
 
 import './App.css';
-
-// const ProtectedRoute = ({ user, component }) => {
-//   if(props.user){
-//     return props.component;
-//   } else {
-//     return <Redirect to="/login" />
-//   }
-// };
 
 function App() {
   
@@ -35,130 +27,56 @@ function App() {
 
   const [ contacts, setContacts ] = useState([]);
 
-  const fetchData = useRef(null);
+  const CONTACTS_API_URL = "http://localhost:3001/contacts"
 
-  const API_URL = "http://localhost:3001/api/contacts"
-
-  const getContacts = async () => {
-    if(!user) return;
-
-  const token = await user.getIdToken();
-  const response = await fetch(API_URL, {
-    method: 'GET',
-    headers: {
-      'Content-type': 'Application/json',
-      'Authorization': 'Bearer' + token
-    }
-  });
-  const contacts = await response.json();
-  setContacts(contacts);
-}
-
-const createContact = async person => {
-  if(!user) return;
-  const token = await user.getIdToken();
-  const data = {...person, managedBy: user.uid}
-  await fetch(API_URL, {
-    method: 'POST', 
-    headers: {
-      'Content-type': 'Application/json',
-      'Authorization': 'Bearer ' + token
-    },
-    body: JSON.stringify(data)
-});
-getContacts();
-}
-
-const createNote = async (note, id) => {
-  if(!user) return;
-  const token = await user.getIdToken();
-  const data = { ...note, createdBy: user.uid };
-  await fetch(`${API_URL}/${id}/notes`, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'Application/json',
-      'Authorization': 'Bearer ' + token
-    },
-    body: JSON.stringify(data)
-  });
-  getContacts();
-}
-
-useEffect(() => {
-  fetchData.current = getContacts;
-});
-
-
-
-  //TODO: add heroku api/url
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setUser(user);
-
-      if(user) {
-        fetchData.current();
-      } else {
-        setContacts([]);
-      }
-      
+  //contacts helper functions
+  const getContacts = async() => {
+    const response = await fetch(CONTACTS_API_URL);
+    const contacts = await response.json();
+    setContacts(contacts);
+  }
+  
+  const createContact = async person => {
+    await fetch(CONTACTS_API_URL, {
+      method: 'POST',
+      headers: {'Content-type': 'Application/json'},
+      body: JSON.stringify(person)
     });
-    
+    getContacts(); // to refresh the list of contacts
+  }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => setUser(user));
+    getContacts();
+    return () => unsubscribe();
+  }, []);
 
-  return () => unsubscribe();
-  }, [user]);
+
 
 
   return (
     <>
-      <Header user={user} />
-      <Nav user={user}/>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/login" render={() => (
-          user ? <Redirect to="/dashboard" /> : <Login />
-        )}>
-        </Route>
-        <Route path="/product">
-          <Product />
-        </Route>
-        <Route path="/solutions">
-          <Solutions />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/dashboard" render={() => (
-          user ? <Dashboard /> : <Redirect to="/login" />
-        )}>
-        </Route>
-        <Route path="/contacts" render={() => (
-          user ? (
-          <Contacts user={user}
-          contacts={contacts} 
-          createContact={createContact} 
-          /> 
-          ) : <Redirect to="/login" />
-        )} >
-        </Route>
-        <Route path="/contacts/:id" render={(props) => (
-            user ? (
-              <Show 
-                contact={contacts.find(c => c._id === props.match.params.id)} 
-                createNote={createNote}
-              />
-            ) : <Redirect to="/login" />
-          )} />
-        <Route path="/applications">
-          <Applications />
-        </Route>
-        <Route path="/roadmap">
-          <Roadmap />
-        </Route>
-      </Switch>
-      <Footer />
+     <Header user={user} />
+     <Switch>
+       <Route exact path="/">
+         <Home />
+       </Route>
+       <Route path="/login" render={() => (
+         user ? <Redirect to="/dashboard" /> : <Login />
+       )} />
+       <Route path="/dashboard" render={() => (
+         user ? <Dashboard /> : <Redirect to="/login" />
+       )} />
+       <Route path="/contacts" render={() => (
+         user ? (
+         <Contacts 
+         contacts={contacts}
+         createContact={createContact} 
+         /> 
+         ) : <Redirect to="/login" />
+       )} />
+     </Switch>
+     <Footer />
+
       </>
   );
 }
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { StyledForm, StyledMain, StyledSection, StyledTable } from '../styles';
+import { StyledMain } from '../styles';
+
 const Contacts = (props) => {
     const [ formState, setFormState ] = useState({
         firstName: "",
@@ -11,107 +11,16 @@ const Contacts = (props) => {
         lastContacted: "",
     });
 
-    const handleChange = event => {
-        setFormState(prevState => ({
-            ...prevState,
-            [event.target.name]: event.target.value
-        }));
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault(); 
-        props.createContact(formState)
-        setFormState({
-        firstName: "",
-        lastName: "",
-        email:"",
-        companyName: "",
-        role: "",
-        lastContacted: "",
-        })
-    }
-
-    // form helper functions 
-
+const handleChange = event => {
+    setFormState(prevState => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+    }));
+}
     return (
         <StyledMain>
-            <h1>Show Contact Page!</h1>
-            
-            <section>
-                <form onSubmit={handleSubmit}>
-                    <input 
-                    onChange={handleChange}
-                    value={formState.firstName} 
-                    name="firstName" 
-                    type="text" 
-                    placeholder="First Name"
-                    />
-                    <input 
-                    onChange={handleChange}
-                    value={formState.lastName} 
-                    name="lastName" 
-                    type="text" 
-                    placeholder="Last Name"
-                    />
-                    <input 
-                    onChange={handleChange}
-                    value={formState.email} 
-                    name="email" 
-                    type="text" 
-                    placeholder="Email"
-                    />
-                    <input 
-                    onChange={handleChange}
-                    value={formState.companyName} 
-                    name="companyName" 
-                    type="text" 
-                    placeholder="Company Name"
-                    />
-                    <input 
-                    onChange={handleChange}
-                    value={formState.role} 
-                    name="role" 
-                    type="text" 
-                    placeholder="Role"
-                    />
-                    <input 
-                    onChange={handleChange}
-                    value={formState.lastContacted} 
-                    name="lastContacted" 
-                    type="text" 
-                    placeholder="Date Last Contacted"
-                    />
-
-
-                    <input type="submit" value="Add Contact" />
-                </form>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Company Name</th>
-                            <th>Role</th>
-                            <th>Last Contacted</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            props.contacts.map(c => (
-                                <tr key={c._id}> 
-                                <td>{c.firstName}</td>
-                                <td>{c.lastName}</td>
-                                <td>{c.email}</td>
-                                <td>{c.companyName}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </section>
+            <h1>Dashboard</h1>
         </StyledMain>
-    );
-};
-
+    )
+}
 export default Contacts;
