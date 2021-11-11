75381bc89f858edadbf9392ae98c0665a3b940a1
alba-molina-nyc <avmanyc@gmail.com>
2021-11-10 22:46:05 -0500

    alt added to images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,11 +12,13 @@ const Home = (props) => {
      
             <h1>Manage everything career based in one place </h1>
             <h3>This board helps you to keep track of internship and job applications, ensuring you never miss an opportunity! <img src='https://i.imgur.com/O20TmdQ.png' 
-           width='50%'/> </h3>
+           width='50%'
+           alt='home1'/> </h3>
             <p> Full CRM and sourcing capabilities to find and connect with the high-quality companies you seek. You can: Discover more companies with direct sourcing. Nurture your applicants through all social channels. Build beautiful career sites optimized for mobile. Incorporate our one-click apply process
             </p>
             <img src='https://i.imgur.com/0WRy28F.png' 
-           width='50%'/>
+           width='50%'
+           alt='home'/>/>
         </StyledMain>
         </>
     );
